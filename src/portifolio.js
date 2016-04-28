a4e9fe5cc3fe8bdbe6550c8d47dd584b34ca739b
Israel <israelhenrique@gmail.com>
2016-04-28 16:42:01 -0300

    add isBalanced method

--- src/portifolio.js
+++ src/portifolio.js
@@ -1,14 +1,26 @@
 'use strict'
 
 class Portifolio {
-  constructor(categoryList, alloc) {
+  constructor(categoryList, alloc, rebalancePercent) {
     this.categoryList = categoryList
     this.value = 0
     this.alloc = alloc
+    this.rebalancePercent = rebalancePercent
 
     categoryList.forEach((item) => {
       this.value = this.value + item.value
     })
+  }
+  isBalanced() {
+    let msg = 'balanced'
+    let percent = 0
+    this.categoryList.forEach((item, index) => {
+      percent = (item.value / this.value) * 100
+      if (Math.abs(this.alloc[index] - percent) > this.rebalancePercent) {
+        msg = 'desbalanced'
+      }
+    })
+    return msg
   }
 }
 
--- test/unit/portifolio-test.js
+++ test/unit/portifolio-test.js
@@ -35,8 +35,47 @@ describe('Portifolio', () => {
       const c2 = new Category('FII', arrayFII)
       const c3 = new Category('RendaFixa', arrayRenda)
       const categoryList = [c1, c2, c3]
-      const portifolio = new Portifolio(categoryList, 0)
+      const portifolio = new Portifolio(categoryList, [40, 20, 20], 5)
+      expect(portifolio.alloc[0]).to.equals(40)
+      expect(portifolio.alloc[1]).to.equals(20)
+      expect(portifolio.alloc[2]).to.equals(20)
+      expect(portifolio.rebalancePercent).to.equals(5)
       expect(portifolio.value).to.equals(17201.84)
+    })
+  })
+  describe('#isBalanced', () => {
+    it('should return if the portifolio is balanced or not', () => {
+      const s1 = new Asset('BOVA11', 1, 80, 52.87, 1)
+      const s2 = new Asset('SMAL11', 1, 67, 47.29, 1)
+      const s3 = new Asset('FMOF11', 2, 3, 108.00, 1)
+      const s4 = new Asset('EURO11', 2, 7, 165.00, 1)
+      const s5 = new Asset('SAAG11', 2, 6, 94.18, 1)
+      const s6 = new Asset('XTED11', 2, 9, 37.75, 1)
+      const s7 = new Asset('BCFF11B', 2, 5, 64.81, 1)
+      const s8 = new Asset('FAED11B', 2, 4, 157.00, 1)
+      const s9 = new Asset('HTMX11B', 2, 2, 87.86, 1)
+      const s10 = new Asset('RendaFixa', 3, 1, 7270, 1)
+      const arrayAcoes = []
+      const arrayFII = []
+      const arrayRenda = []
+      arrayAcoes.push(s1)
+      arrayAcoes.push(s2)
+      arrayFII.push(s3)
+      arrayFII.push(s4)
+      arrayFII.push(s5)
+      arrayFII.push(s6)
+      arrayFII.push(s7)
+      arrayFII.push(s8)
+      arrayFII.push(s9)
+      arrayRenda.push(s10)
+      const c1 = new Category('Acoes', arrayAcoes)
+      const c2 = new Category('FII', arrayFII)
+      const c3 = new Category('RendaFixa', arrayRenda)
+      const categoryList = [c1, c2, c3]
+      const portifolio = new Portifolio(categoryList, [45, 20, 35], 5)
+      expect(portifolio.isBalanced()).to.equals('balanced')
+      portifolio.alloc = [35, 20, 45]
+      expect(portifolio.isBalanced()).to.equals('desbalanced')
     })
   })
 })
