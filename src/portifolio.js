61e1cc8b407748b5748cb6f2dedaf38dd4f1e307
Israel <israelhenrique@gmail.com>
2016-05-03 09:50:21 -0300

    using categories insted of categoryList

--- src/portifolio.js
+++ src/portifolio.js
@@ -1,20 +1,20 @@
 'use strict'
 
 class Portifolio {
-  constructor(categoryList, alloc, rebalancePercent) {
-    this.categoryList = categoryList
+  constructor(categories, alloc, rebalancePercent) {
+    this.categories = categories
     this.value = 0
     this.alloc = alloc
     this.rebalancePercent = rebalancePercent
 
-    categoryList.forEach((item) => {
+    categories.forEach((item) => {
       this.value = this.value + item.value
     })
   }
   isBalanced() {
     let msg = 'balanced'
     let percent = 0
-    this.categoryList.forEach((item, index) => {
+    this.categories.forEach((item, index) => {
       percent = (item.value / this.value) * 100
       if (Math.abs(this.alloc[index] - percent) > this.rebalancePercent) {
         msg = 'desbalanced'
