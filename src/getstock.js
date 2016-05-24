322e560a833af08b3c4a0fa9c7bfdc6c664086b1
Israel <israelhenrique@gmail.com>
2016-05-24 14:45:14 -0300

    get json from yahoo finance

--- src/getstock.js
+++ src/getstock.js
@@ -2,13 +2,15 @@
 
 'use strict'
 
-const url = 'http://finance.yahoo.com/d/quotes.csv?s=BOVA11.SA&f=sl1'
-
+const qAttrib = 'LastTradePriceOnly'
+const stockName = 'BOVA11.SA'
+const env = 'store%3A%2F%2Fdatatables.org%2Falltableswithkeys'
+const query = `select ${qAttrib} from yahoo.finance.quotes where symbol in ( "${stockName}" )`
+const url = `https://query.yahooapis.com/v1/public/yql?q=${query}&format=json&env=${env}&callback=`
 const request = require('request')
+
 request(url, (error, response, body) => {
   if (!error && response.statusCode === 200) {
     console.log(body) // Show the HTML for the Google homepage.
   }
 })
-
-console.log('teste')
--- src/portifoliomanager.js
+++ src/portifoliomanager.js
@@ -15,6 +15,22 @@ class PortifolioManager {
     }
     return msg
   }
+  fixPortifolioBalance(portifolio) {
+    let msg = ''
+    if (!portifolio.isBalanced()) {
+      portifolio.categories.forEach((item, index) => {
+        const percent = (item.value / portifolio.value) * 100
+        const desbalancePercent = portifolio.alloc[index] - percent
+        if (Math.abs(desbalancePercent) > portifolio.rebalancePercent) {
+          msg += `Category ${item.name} needs ${desbalancePercent / 100 * portifolio.value}`
+          msg += ' to be balanced\n'
+        }
+      })
+    } else {
+      msg = 'Portifolio is balanced\n'
+    }
+    return msg
+  }
 }
 
 module.exports = PortifolioManager
