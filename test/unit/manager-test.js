99e60f483deb653dbe674f55728530104394a454
Israel <israelhenrique@gmail.com>
2016-05-24 15:21:51 -0300

    add test

--- test/unit/manager-test.js
+++ test/unit/manager-test.js
@@ -28,4 +28,17 @@ describe('PortifolioManager', () => {
       expect(portifoliomanager.checkPortifolioBalance(portifolio)).to.equals(msg)
     })
   })
+  describe('#fixPortifolioBalance', () => {
+    it('check a desbalanced portifolio', () => {
+      const arrayAcoes = []
+      const c1 = new Category('Acoes', arrayAcoes)
+      c1.value = 40
+      const categoryList = [c1]
+      const portifolio = new Portifolio(categoryList, [50], 5)
+      portifolio.value = 100
+      const portifoliomanager = new PortifolioManager()
+      const msg = 'Category Acoes needs 10 to be balanced\n'
+      expect(portifoliomanager.fixPortifolioBalance(portifolio)).to.equals(msg)
+    })
+  })
 })
