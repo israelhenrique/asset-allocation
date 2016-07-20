04d682fd7144a338f6a7ef7f6f638f1b85fd343e
Israel Lima <israelhenrique@gmail.com>
2016-07-20 15:26:14 -0300

    add GetStock class

--- src/getstock.js
+++ src/getstock.js
@@ -1,20 +1,21 @@
-#!/usr/bin/env node
-
 'use strict'
 
-const qAttrib = 'LastTradePriceOnly'
-const stockName = 'BOVA11.SA'
-const env = 'store%3A%2F%2Fdatatables.org%2Falltableswithkeys'
-const query = `select ${qAttrib} from yahoo.finance.quotes where symbol in ( "${stockName}" )`
-const url = `https://query.yahooapis.com/v1/public/yql?q=${query}&format=json&env=${env}&callback=`
 const request = require('request')
 
-console.log(url)
+class GetStock {
+  getPrice(stockCode) {
+    const qAttrib = 'LastTradePriceOnly'
+    const env = 'store%3A%2F%2Fdatatables.org%2Falltableswithkeys'
+    const query = `select ${qAttrib} from yahoo.finance.quotes where symbol in ( "${stockCode}" )`
+    const link = `https://query.yahooapis.com/v1/public/yql?q=${query}&format=json&env=${env}&callback=`
+    const options = { url: link, json: true }
 
-request(url, (error, response, body) => {
-  if (!error && response.statusCode === 200) {
-    console.log(body) // Se fizer body.query da ruim
+    request(options, (error, response, json) => {
+      if (!error && response.statusCode === 200) {
+        return json.query.results.quote.LastTradePriceOnly
+      }
+    })
   }
-})
-const json = {"query":{"count":1,"created":"2016-07-14T21:04:35Z","lang":"en-US","results":{"quote":{"LastTradePriceOnly":"53.61"}}}};
-console.log(json.query)
+}
+
+module.exports = GetStock
